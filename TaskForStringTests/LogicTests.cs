4caed4bdad477bbdb07c9de72a075395e45af6fe
neurolius <ewgensafonow@gmail.com>
2026-03-03 23:06:48 +0800

    Создан класс тестов для методов класса Logic

---
+++ TaskForStringTests/LogicTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TaskForString;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskForString.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void CalculateLetterPercentTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
