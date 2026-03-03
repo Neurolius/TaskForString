b08cc7b8c4d5c23e14ebd41b58f2f339ec253a13
neurolius <ewgensafonow@gmail.com>
2026-03-03 23:15:08 +0800

    Созданы тесты для методов класса Logic

--- TaskForStringTests/LogicTests.cs
+++ TaskForStringTests/LogicTests.cs
@@ -11,10 +11,77 @@ namespace TaskForString.Tests
     [TestClass()]
     public class LogicTests
     {
+
         [TestMethod()]
-        public void CalculateLetterPercentTest()
+        public void CalculateLetterPercentOnlyLetters()
         {
-            Assert.Fail();
+            var sentence = new Sentence { Text = "Hello" };
+
+            double result = Logic.CalculateLetterPercent(sentence);
+
+            Assert.AreEqual(100, result);
+        }
+
+        [TestMethod()]
+        public void CalculateLetterPercentWithSymbols()
+        {
+            var sentence = new Sentence { Text = "Hello!!!" };
+
+            double result = Logic.CalculateLetterPercent(sentence);
+
+            Assert.AreEqual(62.5, result);
+        }
+
+        [TestMethod()]
+        public void CalculateLetterPercentEmptyString()
+        {
+            var sentence = new Sentence { Text = "" };
+
+            double result = Logic.CalculateLetterPercent(sentence);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod()]
+        public void CalculateEachLetterPercentSimpleWord()
+        {
+            var sentence = new Sentence { Text = "aab" };
+
+            var result = Logic.CalculateEachLetterPercent(sentence);
+
+            Assert.AreEqual(66, result["a"]);
+            Assert.AreEqual(33, result["b"]);
+        }
+
+        [TestMethod()]
+        public void CalculateEachLetterPercentIgnoreCase()
+        {
+            var sentence = new Sentence { Text = "AaA" };
+
+            var result = Logic.CalculateEachLetterPercent(sentence);
+
+            Assert.AreEqual(100, result["a"]);
+        }
+
+        [TestMethod()]
+        public void CalculateEachLetterPercentWithSymbols()
+        {
+            var sentence = new Sentence { Text = "a!a?b" };
+
+            var result = Logic.CalculateEachLetterPercent(sentence);
+
+            Assert.AreEqual(40, result["a"]);
+            Assert.AreEqual(20, result["b"]);
+        }
+
+        [TestMethod()]
+        public void CalculateEachLetterPercentNoLetters()
+        {
+            var sentence = new Sentence { Text = "12345!!!" };
+
+            var result = Logic.CalculateEachLetterPercent(sentence);
+
+            Assert.AreEqual(0, result.Count);
         }
     }
 }
