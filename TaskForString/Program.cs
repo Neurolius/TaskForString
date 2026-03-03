78905c3e41c78a674c4495bbb59b487ba725366e
neurolius <ewgensafonow@gmail.com>
2026-03-03 22:46:28 +0800

    Создан класс для работы с предложениями

--- TaskForString/Program.cs
+++ TaskForString/Program.cs
@@ -1,5 +1,31 @@
 ﻿namespace TaskForString
 {
+    class Sentence
+    {
+        public string Text { get; set; }
+        
+        public int GetLetterCount()
+        {
+            int count = 0;
+            foreach (char c in Text)
+            {
+                if (char.IsLetter(c))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public int GetLength()
+        {
+            return Text?.Length??0;
+        }
+        public string GetLowerText()
+        {
+            return Text?.ToLower()??string.Empty;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
